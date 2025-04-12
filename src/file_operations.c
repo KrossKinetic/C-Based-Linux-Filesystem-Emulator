2bc33564b4141fdc58c9c802446eb84365b54aff
Anshumaan (Manas) Singh <83864851+KrossKinetic@users.noreply.github.com>
2025-04-12 02:46:33 +0000

    Partially done with Part 2

--- src/file_operations.c
+++ src/file_operations.c
@@ -7,6 +7,181 @@
 #define DIRECTORY_ENTRY_SIZE (sizeof(inode_index_t) + MAX_FILE_NAME_LEN)
 #define DIRECTORY_ENTRIES_PER_DATABLOCK (DATA_BLOCK_SIZE / DIRECTORY_ENTRY_SIZE)
 
+// ----------------------- HELPER FUNCTION --------------------- //
+
+void format_token_for_comparison(char* token, char formatted_name[14]) {
+    if (token == NULL) {
+        memset(formatted_name, 0, 14);
+        return;
+    }
+    size_t len = strlen(token);
+    if (len >= 14) {
+        memcpy(formatted_name, token, 14);
+    } else {
+        strcpy(formatted_name, token);
+        memset(formatted_name + len + 1, ' ', 14 - len - 1);
+    }
+}
+
+char* format_filename(char* filename) {
+    if (filename == NULL) {
+        return NULL;
+    }    
+    size_t len = strlen(filename);
+    if (len < 14) {
+        memset(filename + len + 1, ' ', 14 - len - 1);
+    }
+    return filename;
+}
+
+int count_char_occurrences(const char *str, char target) {
+    int count = 0;
+    if (str != NULL) {
+        for (int i = 0; str[i] != '\0'; i++) {
+            if (str[i] == target) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+inode_t* return_inode(char *path, inode_t* dir, filesystem_t* fs){
+    int slashes = count_char_occurrences(path,'/');
+    char * token = strtok(path, "/");
+    char formatted_name[14];
+    format_token_for_comparison(token,formatted_name);
+    inode_t *cur_inode = dir;
+    int is_file = 0;
+    size_t bytes_read = 0;
+    while(token != NULL && slashes>=0) {
+        
+        if (slashes == 0){
+            is_file = 1;
+        }
+        
+        byte *all_data = malloc(cur_inode->internal.file_size);
+        inode_read_data(fs,cur_inode,0,all_data,cur_inode->internal.file_size,&bytes_read);
+        byte *original_ptr = all_data;
+        for (size_t i = 0; i < (bytes_read)/16; i++){
+            inode_index_t inode_inside_idx;
+            memcpy(&inode_inside_idx,all_data,2);
+            all_data += 2;
+
+            char name_of_inode[14];
+            memcpy(name_of_inode, all_data, 14);
+            format_filename(name_of_inode);
+            all_data+=14;
+
+            if (is_file == 0){
+                if (strcmp(formatted_name,name_of_inode) == 0){
+                    inode_t *next_inode = &fs->inodes[inode_inside_idx];
+                    cur_inode = next_inode;
+                    break;
+                }
+            } else {
+                if (strcmp(formatted_name,name_of_inode) == 0){
+                    inode_t *next_inode = &fs->inodes[inode_inside_idx];
+                    return next_inode;
+                }
+            }
+
+        }
+        format_token_for_comparison(strtok(NULL, "/"),formatted_name);
+        free(original_ptr);
+        slashes--;
+    }
+    return NULL;
+}
+
+int verify_path(char *path, inode_t* dir, filesystem_t* fs){
+    int slashes = count_char_occurrences(path,'/');
+    
+    char * token = strtok(path, "/");
+    char formatted_name[14];
+    format_token_for_comparison(token,formatted_name);
+    
+    inode_t *cur_inode = dir;
+    
+    int is_file = 0;
+    int file_found = 0;
+
+    size_t bytes_read = 0;
+
+    //int count = 0;
+    while(token != NULL && slashes>=0) {
+
+        if (slashes == 0){
+            is_file = 1;
+        }
+
+        byte *all_data = malloc(cur_inode->internal.file_size);
+        inode_read_data(fs,cur_inode,0,all_data,cur_inode->internal.file_size,&bytes_read);
+        byte *original_ptr = all_data;
+
+        for (size_t i = 0; i < (bytes_read)/16; i++){
+            inode_index_t inode_inside_idx;
+            memcpy(&inode_inside_idx,all_data,2);
+            all_data += 2;
+
+            char name_of_inode[14];
+            memcpy(name_of_inode, all_data, 14);
+            format_filename(name_of_inode);
+            all_data+=14;
+
+            if (is_file == 0){
+                if (strcmp(formatted_name,name_of_inode) == 0){
+                    inode_t *next_inode = &fs->inodes[inode_inside_idx];
+                    if (next_inode->internal.file_type == DIRECTORY){
+                        cur_inode = next_inode;
+                        file_found = 1;
+                        break;
+                    } else {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+            } else {
+                if (strcmp(formatted_name,name_of_inode) == 0){
+                    inode_t *next_inode = &fs->inodes[inode_inside_idx];
+                    
+                    if (next_inode->internal.file_type != DATA_FILE){
+                        REPORT_RETCODE(INVALID_FILE_TYPE);
+                        return 0;
+                    }
+
+                    if (next_inode->internal.file_type == DATA_FILE){
+                        cur_inode = next_inode;
+                        file_found = 1;
+                        break;
+                    } else {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+            }
+
+        }
+
+        if (file_found == 0 && is_file == 0){
+            REPORT_RETCODE(DIR_NOT_FOUND);
+            return 0;
+        } else if (file_found == 0 && is_file == 1){
+            REPORT_RETCODE(FILE_NOT_FOUND);
+            return 0;
+        }
+
+        format_token_for_comparison(strtok(NULL, "/"),formatted_name);
+
+        free(original_ptr);
+        slashes--;
+        file_found = 0;
+    }
+    return 1;
+}
+
 // ----------------------- CORE FUNCTION ----------------------- //
 int new_file(terminal_context_t *context, char *path, permission_t perms)
 {
@@ -69,28 +244,42 @@ int tree(terminal_context_t *context, char *path)
 //Part 2
 void new_terminal(filesystem_t *fs, terminal_context_t *term)
 {
-    (void) fs;
-    (void) term;
-    //check if inputs are valid
-
-    //assign file system and root inode.
+    if (fs==NULL) return;
+    term->fs = fs;
+    term->working_directory = &(fs->inodes[0]);
 }
 
 fs_file_t fs_open(terminal_context_t *context, char *path)
 {
-    (void) context;
-    (void) path;
+    char* path_copy = NULL;
+    if (path != NULL) path_copy = strdup(path);
 
-    //confirm path exists, leads to a file
-    //allocate space for the file, assign its fs and inode. Set offset to 0.
-    //return file
+    if (context==NULL){
+        return NULL;
+    }
+    
+    if (path==NULL){
+        REPORT_RETCODE(DIR_NOT_FOUND);
+        return NULL;
+    }
 
-    return (fs_file_t)0;
+    if (verify_path(path,context->working_directory,context->fs) == 0){
+        return NULL;
+    }
+
+    fs_file_t file = (struct fs_file*)malloc(sizeof(struct fs_file));
+
+    file->offset = 0;
+    file->inode = return_inode(path_copy, context->working_directory, context->fs);
+    file->fs = context->fs;
+
+    return file;
 }
 
 void fs_close(fs_file_t file)
 {
-    (void)file;
+    if (file == NULL) return;
+    free(file);
 }
 
 size_t fs_read(fs_file_t file, void *buffer, size_t n)
